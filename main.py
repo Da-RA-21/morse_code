7b09daa53a5eb47b847309caac3528764ce57f96
Da_RA_21 <rashawnthompson@unomaha.edu>
2022-05-12 17:05:45 -0500

    added comment telling where I got the project idea and what I added to it

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from controller import *
 
+"""
+project idea: https://www.youtube.com/watch?v=delJJ9zHXio&t=140s
+added: slightly expanded library to handle more characters, simple GUI app to gather input from user,
+       time is handled a little differently from the originally for a more real feel in my opinion
+"""
+
 def main():
     app = QApplication([])
     window = Controller()
